cf0163a9c35f98b4b99301cf521b741afb9054fd
JeanStkng <jeancast.jg@gmail.com>
2024-03-22 22:15:15 -0500

    Refactor and few improvements in realtime interactions with game entities

--- src/objects/Globe.js
+++ src/objects/Globe.js
@@ -1,15 +1,20 @@
 import { v4 as uuidv4 } from "uuid";
 
 class Globe {
-  constructor(game, x, y) {
-    this.userId = uuidv4();
+  constructor(game, x, y, id, isMainPlayer = true) {
+    this.userId = id ?? uuidv4();
     this.score = 1;
     this.initialSize = 36;
     this.zoomDisminution = 0.01;
     this.speed = 200;
+    this.timeElapsed = 0;
+    this.timeOut = 50;
+    this.canSendMovement = false;
     this.game = game;
+    this.client = game.mainClient;
     this.gameObject;
     this.actualPointerPos;
+    this.isMainPlayer = isMainPlayer;
 
     if (x) {
       this.x = x;
@@ -32,16 +37,95 @@ class Globe {
       .setCollideWorldBounds(true)
       .setDepth(this.score);
 
-    this.gameObject.body.onCollide = true;
+    this.gameObject.body.onOverlap = true;
 
-    this.game.input.on("pointermove", (pointer) => {
-      this.actualPointerPos = this.game.cam.getWorldPoint(pointer.x, pointer.y);
-      this.game.physics.moveToObject(
-        this.gameObject,
-        this.actualPointerPos,
-        this.speed
+    if (this.isMainPlayer) {
+      this.cam = this.game.cameras.main;
+
+      this.cam.setBounds(0, 0, 4096, 4096).setZoom(2);
+
+      this.cam.startFollow(this.gameObject);
+
+      this.game.input.on("pointermove", (pointer) => {
+        this.actualPointerPos = this.cam.getWorldPoint(pointer.x, pointer.y);
+        this.game.physics.moveToObject(
+          this.gameObject,
+          this.actualPointerPos,
+          this.speed
+        );
+      });
+
+      this.game.physics.world.on(
+        "overlap",
+        async (gameObject1, gameObject2, _body1, _body2) => {
+          switch (gameObject2.name) {
+            case "cell":
+              const cellId = gameObject2.getData("id");
+              gameObject2.destroy();
+
+              this.game.cells = this.game.cells.filter(
+                (val) => val.id !== cellId
+              );
+
+              this.score += 0.05;
+
+              this.gameObject.setDisplaySize(
+                this.initialSize * this.score,
+                this.initialSize * this.score
+              );
+              this.gameObject.setCircle(this.initialSize * this.score);
+
+              if (this.cam.zoom > 1) {
+                this.speed -= 0.025;
+                this.cam.setZoom(this.cam.zoom - this.zoomDisminution);
+              }
+
+             await this.client
+                .from("cells")
+                .update({
+                  isEaten: true,
+                })
+                .match({
+                  id: cellId,
+                });
+              break;
+
+            case "globe":
+              if (gameObject1.depth > gameObject2.depth) {
+                console.log("El diablaso");
+              }
+
+              break;
+
+            default:
+              break;
+          }
+        }
       );
-    });
+    }
+  }
+
+  async update(isConnected, delta) {
+    if (this.timeElapsed >= this.timeOut) {
+      this.timeElapsed = 0;
+      this.canSendMovement = true;
+    } else {
+      this.timeElapsed += delta;
+      this.canSendMovement = false;
+    }
+
+    if (isConnected && this.userId && this.canSendMovement) {
+      await this.client
+        .from("players")
+        .update({
+          x: this.gameObject.x,
+          y: this.gameObject.y,
+          score: this.score,
+        })
+        .match({
+          id: this.userId,
+        });
+    }
   }
 }
 
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,24 +5,18 @@ import Globe from "../objects/Globe";
 export class Game extends Scene {
   globe;
   cursor;
-  cam;
-  isConnected = false;
-  channelA;
-  clientMain;
-  userId;
   users = [];
-  timeElapsed = 0;
-  timeOut = 50;
-  canSendMovement = false;
+  cells = [];
+  isConnected = false;
 
   constructor() {
     super("Game");
-    this.globe = new Globe(this);
 
     // Initialize Client and Room from Supabase Realtime Channel
     const { room, client } = getRealtimeChannel();
     this.channelA = room;
-    this.clientMain = client;
+    this.mainClient = client;
+    this.globe = new Globe(this);
   }
 
   create() {
@@ -34,7 +28,6 @@ export class Game extends Scene {
 
     const userStatus = {
       id: this.globe.userId,
-      isConnected: true,
       initialX: this.globe.gameObject.x,
       initialY: this.globe.gameObject.y,
     };
@@ -43,82 +36,60 @@ export class Game extends Scene {
       .on("presence", { event: "sync" }, () => {
         const newState = this.channelA.presenceState();
 
-        /* Find in the local users the user that has the same id from the state received
-           consider that each state has a key then data is inside an array and always
-           on first position */
-        const foundUsr = Object.keys(newState).find((key) =>
-          this.users.find((user) => newState[key][0].id === user.id)
+        const foundUsr = this.users.find((user) =>
+          Object.keys(newState).find(
+            (key) =>
+              newState[key][0].id === user.id &&
+              newState[key][0].id !== this.globe.userId
+          )
         );
 
         if (!foundUsr && Object.keys(newState).length > 1) {
-          const newBall = this.physics.add
-            .image(newState.initialX, newState.initialY, "ball")
-            .setDisplaySize(36, 36)
-            .setCircle(36)
-            .setCollideWorldBounds(true);
+          Object.keys(newState).map((key) => {
+            const usrState = newState[key][0];
+            if (usrState.id === this.globe.userId) return;
 
-          newBall.this.physics.add.collider(newBall);
+            const newBall = new Globe(
+              this,
+              usrState.initialX,
+              usrState.initialY,
+              usrState.id,
+              false
+            );
+            newBall.initialize();
 
-          this.users.push({
-            id: newState.user,
-            userGlobe: newBall,
-            x: newState.initialX,
-            y: newState.initialY,
+            this.physics.add.overlap(this.globe.gameObject, newBall.gameObject);
+
+            this.users.push({
+              id: usrState.id,
+              userGlobe: newBall.gameObject,
+              x: usrState.initialX,
+              y: usrState.initialY,
+            });
           });
         }
       })
       .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-        const usersToLeave = this.users.filter(
-          (user) => user.id === leftPresences.id
+        const usersToLeave = this.users.filter((user) =>
+          leftPresences.find((elem) => elem.id === user.id)
         );
 
         if (usersToLeave.length > 0) {
-          this.usersToLeave.map((usr) => usr.userGlobe.destroy());
-          this.users = this.users.filter(
-            (user) => user.id !== leftPresences.id
+          usersToLeave.map((usr) => usr.userGlobe.destroy());
+          this.users = this.users.filter((user) =>
+            leftPresences.find((elem) => elem.id !== user.id)
           );
         }
       })
       .on(
         "postgres_changes",
         { event: "*", schema: "public", table: "players" },
-        (payload) => {
-          const { x, y, id } = payload.new;
-
-          if (payload.eventType === "UPDATE") {
-            this.users.map((usr) => {
-              if (usr.id === id) {
-                usr.x = x;
-                usr.y = y;
-              }
-            });
-          }
-
-          if (
-            payload.eventType === "INSERT" &&
-            payload.new.id !== this.globe.userId
-          ) {
-            const foundUser = this.users.find(
-              (user) => user.id === payload.new.id
-            );
-
-            if (!foundUser) {
-              const newBall = new Globe(this, x, y);
-              newBall.initialize();
-
-              this.users.push({
-                id: id,
-                userGlobe: newBall.gameObject,
-                x: x,
-                y: y,
-              });
-            } else {
-              foundUser.x = x;
-              foundUser.y = y;
-              // this.physics.moveTo(foundUser.userGlobe, payload.x, payload.y, 200);
-            }
-          }
-        }
+        (payload) => this.handlePlayersTableChanges(payload)
+      )
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "cells" },
+        (payload) => this.handleCellsTableUpdate(payload)
       )
       .subscribe(async (status) => {
         if (status !== "SUBSCRIBED") {
@@ -128,14 +99,14 @@ export class Game extends Scene {
         }
 
         await this.channelA.track(userStatus); // Send presence status
-        await this.clientMain.from("players").insert({
+        await this.mainClient.from("players").insert({
           id: this.globe.userId,
           x: this.globe.gameObject.x,
           y: this.globe.gameObject.y,
           score: this.globe.score,
         });
 
-        const { data } = await this.clientMain.from("cells").select();
+        const { data } = await this.mainClient.from("cells").select();
         data.map((cell) => {
           if (!cell.isEaten) {
             const newCell = this.physics.add
@@ -148,7 +119,8 @@ export class Game extends Scene {
             newCell.setDataEnabled();
             newCell.setData("id", cell.id);
 
-            this.physics.add.collider(this.globe.gameObject, newCell);
+            this.physics.add.overlap(this.globe.gameObject, newCell);
+            this.cells.push({ id: cell.id, gameObject: newCell });
           }
         });
 
@@ -156,87 +128,101 @@ export class Game extends Scene {
         this.events.emit("connection", this.isConnected);
       });
 
-    this.cam = this.cameras.main;
-
-    this.cam.setBounds(0, 0, 4096, 4096).setZoom(2);
     this.physics.world.setBounds(100, 100, 4000, 4000);
 
-    this.cam.startFollow(this.globe.gameObject);
-
     this.cursor = this.add.rectangle(0, 0, 20, 20);
-
-    this.physics.world.on(
-      "collide",
-      async (gameObject1, gameObject2, body1, body2) => {
-        if (gameObject2.name === "cell") {
-          const cellId = gameObject2.getData("id");
-          gameObject2.destroy();
-
-          this.globe.score += 0.05;
-
-          this.globe.gameObject.setDisplaySize(
-            this.globe.initialSize * this.globe.score,
-            this.globe.initialSize * this.globe.score
-          );
-          this.globe.gameObject.setCircle(
-            this.globe.initialSize * this.globe.score
-          );
-
-          if (this.cam.zoom > 1) {
-            this.globe.speed -= 0.025;
-            this.cam.setZoom(this.cam.zoom - this.globe.zoomDisminution);
-          }
-
-          this.clientMain
-            .from("cells")
-            .update({
-              isEaten: true,
-            })
-            .match({
-              id: cellId,
-            });
-        }
-      }
-    );
   }
 
-  async update(_time, delta) {
-    if (this.timeElapsed >= this.timeOut) {
-      this.timeElapsed = 0;
-      this.canSendMovement = true;
-    } else {
-      this.timeElapsed += delta;
-      this.canSendMovement = false;
-    }
-
+  update(_time, delta) {
     this.users
       .filter((val) => val.id !== this.globe.userId)
       .map((usr) => {
-        if (
-          Phaser.Math.Distance.Between(
-            usr.userGlobe.x,
-            usr.userGlobe.y,
-            usr.x,
-            usr.y
-          ) <= 10
-        ) {
-          usr.userGlobe.body.reset(usr.x, usr.y);
-        } else {
-          this.physics.moveTo(usr.userGlobe, usr.x, usr.y, 200);
-        }
+        // if (
+        //   Phaser.Math.Distance.Between(
+        //     usr.userGlobe.x,
+        //     usr.userGlobe.y,
+        //     usr.x,
+        //     usr.y
+        //   ) <= 2
+        // ) {
+        //   usr.userGlobe.body.reset(usr.x, usr.y);
+        // } else {
+        this.physics.moveTo(usr.userGlobe, usr.x, usr.y, 200);
+        // }
       });
 
-    if (this.isConnected && this.globe.userId && this.canSendMovement) {
-      await this.clientMain
-        .from("players")
-        .update({
-          x: this.globe.gameObject.x,
-          y: this.globe.gameObject.y,
-          score: this.globe.score,
-        })
-        .match({
-          id: this.globe.userId,
+    this.globe.update(this.mainClient, this.isConnected, delta);
+  }
+
+  handlePlayersTableChanges(payload) {
+    const { x, y, id, score } = payload.new;
+
+    switch (payload.eventType) {
+      case "UPDATE":
+        this.users.map((usr) => {
+          if (usr.id === id && this.globe.userId !== id) {
+            usr.x = x;
+            usr.y = y;
+            usr.userGlobe.setDepth(score);
+
+            usr.userGlobe.setDisplaySize(
+              this.globe.initialSize * score,
+              this.globe.initialSize * score
+            );
+            usr.userGlobe.setCircle(this.globe.initialSize * score);
+          }
         });
+        break;
+
+      case "INSERT":
+        if (payload.new.id !== this.globe.userId) {
+          const foundUser = this.users.find(
+            (user) => user.id === payload.new.id
+          );
+
+          if (foundUser) {
+            foundUser.x = x;
+            foundUser.y = y;
+            return;
+          }
+
+          const newBall = new Globe(this, x, y, id, false);
+          newBall.initialize();
+
+          this.physics.add.overlap(this.globe.gameObject, newBall.gameObject);
+
+          this.users.push({
+            id: id,
+            userGlobe: newBall.gameObject,
+            x: x,
+            y: y,
+          });
+        }
+        break;
+    }
+  }
+
+  handleCellsTableUpdate(payload) {
+    const { id, isEaten, x, y } = payload.new;
+
+    const existingCell = this.cells.find((cell) => cell.id === id);
+
+    if (existingCell && isEaten) {
+      this.cells = this.cells.filter((cell) => cell.id !== id);
+      existingCell.gameObject.destroy();
+    } else if (!isEaten) {
+      const newCell = this.physics.add
+        .image(x, y, "food")
+        .setName("cell")
+        .setDisplaySize(24, 24)
+        .setCircle(24)
+        .setCollideWorldBounds(true);
+
+      newCell.setDataEnabled();
+      newCell.setData("id", id);
+
+      this.physics.add.overlap(this.globe.gameObject, newCell);
+      this.cells.push({ id: id, gameObject: newCell });
     }
   }
 }
